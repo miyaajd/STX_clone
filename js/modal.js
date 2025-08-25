8f84ad19d8a016b8213b1e96c54c9801c05e0f73
miyaajd <ehdal1032@gmail.com>
2025-08-25 11:59:36 +0900

    modal done

---
+++ js/modal.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function(){
+    // 모달창 닫기
+    const modal = this.document.querySelector(".modal-wrap")
+    const close = this.document.querySelector(".modalClose")
+    close.addEventListener("click",()=>{
+        modal.style.display = "none"
+    })
+})
