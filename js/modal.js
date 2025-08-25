26075cc5eac68391cef5104ecdc61db81b7cad8b
miyaajd <ehdal1032@gmail.com>
2025-08-25 12:42:30 +0900

    quick menu edit

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,17 @@
-window.addEventListener("load", function(){
-    // 모달창 닫기
-    const modal = this.document.querySelector(".modal-wrap")
-    const close = this.document.querySelector(".modalClose")
-    close.addEventListener("click",()=>{
-        modal.style.display = "none"
-    })
-    // visual Btn 활성화
-    
-})
+window.addEventListener("load", function () {
+  // 모달창 닫기
+  const modal = this.document.querySelector(".modal-wrap");
+  const close = this.document.querySelector(".modalClose");
+  close.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+  // scroll event
+  const goTop = this.document.querySelector("#goTop");
+  this.window.addEventListener("scroll", () => {
+    if (this.document.documentElement.scrollTop > 1000) {
+      goTop.style.display = "block";
+    }else{
+        goTop.style.display = "none";
+    }
+  });
+});
